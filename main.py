d9e7d276383bec7b42deaabc2211b5c46496b74f
Byte Me <chemerilcollins@gmail.com>
2023-10-21 12:38:16 +0200

    Optimize the smartlight and fix minor bugs

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ class Device:
         self.__device_id = device_id
         self.__status = "off"
 
-    def get_status(self):
+    @property
+    def status(self):
         return self.__status
 
-    def get_id(self):
+    @property
+    def id(self):
         return self.__device_id
 
     def turn_on(self):
@@ -22,15 +24,27 @@ class Device:
 
 
 class SmartLight(Device):
-    def __init__(self, device_id, brightness):
+    def __init__(self, device_id):
         super().__init__(device_id)
-        self.__brightness = brightness
+        self.__brightness = 0
 
-    def get_brightness(self):
+    @property
+    def brightness(self):
         return self.__brightness
 
-    def adjust_brightness(self, brightness):
+    def adjust_brightness(self, brightness: int):
+        if brightness > 100 or brightness < 0:
+            raise LightException(
+                "Brightness level cannot be more than 100 or less than 0"
+            )
+        if brightness > 0:
+            self.turn_on()
         self.__brightness = brightness
+        if brightness == 0:
+            self.turn_off()
+
+    def __str__(self) -> str:
+        return f"Device: Smart Light Device Id: {self.id} Brightness: {self.brightness} Status: {self.status}"
 
 
 class Thermostat(Device):
@@ -56,34 +70,33 @@ class SecurityCamera(Device):
 
 class AutomationSystem:
     def __init__(self):
-        self.__cameras = []
-        self.__thermostats = []
-        self.__lights = []
+        self.cameras = []
+        self.thermostats = []
+        self.lights = []
 
     def discover_devices(self):
         pass
 
-    def add_camera(self, cam: SecurityCamera):
-        self.__cameras.append(cam)
-
-    def add_light(self, light: SmartLight):
-        self.__lights.append(light)
-
-    def add_thermostat(self, thermostat: Thermostat):
-        self.__thermostats.append(thermostat)
+    def add_device(self, device: Device):
+        if isinstance(device, SecurityCamera):
+            self.cameras.append(device)
+        elif isinstance(device, Thermostat):
+            self.thermostats.append(device)
+        elif isinstance(device, SmartLight):
+            self.lights.append(device)
 
     def turn_lights_automatically(self, camera_id):
-        for camera in self.__cameras:
-            if camera.get_id() == camera_id:
+        for camera in self.cameras:
+            if camera.id() == camera_id:
                 if camera.get_security_status() == "Detected motion":
-                    for light in self.__lights:
-                        light.turn_on()
+                    for light_bulb in self.lights:
+                        light_bulb.turn_on()
                 else:
-                    for light in self.__lights:
-                        light.turn_off()
+                    for light_bulb in self.lights:
+                        light_bulb.turn_off()
 
     def randomization(self):
-        for dev in self.__lights and self.__cameras:
+        for dev in self.lights and self.cameras:
             if isinstance(SmartLight, dev):
                 brightness = random.randint(0, 100)
                 dev.adjust_brightness(brightness)
@@ -95,3 +108,22 @@ class AutomationSystem:
             elif isinstance(SecurityCamera, dev):
                 status = random.choice(["Detected motion", "No motion"])
                 dev.set_security_status(status)
+
+
+class LightException(Exception):
+    pass
+
+
+auto = AutomationSystem()
+cam = SecurityCamera("Camera1")
+light = SmartLight("Light1")
+thermo = Thermostat("Thermostat1")
+print(light.__str__())
+light.adjust_brightness(40)
+print(light.__str__())
+light.adjust_brightness(0)
+print(light.__str__())
+
+auto.add_device(cam)
+auto.add_device(light)
+auto.add_device(thermo)
